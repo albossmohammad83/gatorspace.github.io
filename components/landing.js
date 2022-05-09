bf141435c18a42fda5c723183c266bf65d134ba1
seongjungkim1007 <skim53@sfsu.edu>
2022-05-08 20:11:18 -0700

    my commit

--- components/landing.js
+++ components/landing.js
@@ -5,18 +5,23 @@ function LoginLogout({ props }) {
   if (!props.sStorage && !props.lStorage) {
     return (
       <div id="base" className={"flex-box"}>
+        <div id ="buttons-container">
+        
       <div id="button-left">
-        <Button size={"large"} href="/login">
+      
+        <Button size={"small"} href="/login">
           Sign in
         </Button>
-        <br />
+        {/* <br /> */}
       </div>
       <div id="button-right">
-        <Button size={"large"} href="/registration">
+        <Button size={"small"} href="/registration">
           Sign up
         </Button>
-        <br />
+        {/* <br /> */}
       </div>
+      
+    </div>
     </div>
     );
   }
@@ -26,7 +31,7 @@ export default function Landing({ isLoggedIn }) {
   return (
     <div id="base">
     <div id="Title">Gator space</div>
-    {/* <LoginLogout props={isLoggedIn} /> */}
+    <LoginLogout props={isLoggedIn} />
     </div>
   );
 }
