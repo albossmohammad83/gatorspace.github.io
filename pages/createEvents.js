2426d11b0869cea10d3e8978067e7c59dd9d51eb
Chi Tran <ttranpham@mail.sfsu.edu>
2022-05-09 22:57:59 -0700

    styles

--- pages/createEvents.js
+++ pages/createEvents.js
@@ -23,9 +23,9 @@ return(
       <div id="addFreind-content">
 <div class="topnav">
   <div class="search-container">
-    <form action="/action_page.php">
-      <input type="text" placeholder="Search.." name="search"></input>
-      <button type="submit">Submit</button>
+    <form id="nav-form" action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search" id="nav-search-input"></input>
+      <button type="submit" id="nav-search-button">Submit</button>
     </form>
   </div>
 </div>
